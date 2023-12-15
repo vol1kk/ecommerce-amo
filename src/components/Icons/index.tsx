369ef5c2d5ebd62fad461632db961837661367a2
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-12-15 13:55:30 +0200

    feat: added icons for feature sidebar nav

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,8 +1,11 @@
+export { default as ManIcon } from "@/components/Icons/ManIcon";
 export { default as StarIcon } from "@/components/Icons/StarIcon";
 export { default as UserIcon } from "@/components/Icons/UserIcon";
 export { default as CartIcon } from "@/components/Icons/CartIcon";
 export { default as LogoIcon } from "@/components/Icons/LogoIcon";
+export { default as WomanIcon } from "@/components/Icons/WomanIcon";
 export { default as HeartIcon } from "@/components/Icons/HeartIcon";
 export { default as ArrowIcon } from "@/components/Icons/ArrowIcon";
+export { default as UnisexIcon } from "@/components/Icons/UnisexIcon";
 export { default as SearchIcon } from "@/components/Icons/SearchIcon";
 export { default as ChevronIcon } from "@/components/Icons/ChevronIcon";
