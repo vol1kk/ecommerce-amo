413992ea8f30c87904085d0a724711d11443c5a8
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-12-20 22:37:17 +0200

    feat: don't show "or" if no oauth providers

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -14,16 +14,20 @@ export default function Page() {
         className="aspect-square w-full object-cover"
       />
       <section className="grid flex-1 place-content-center  p-4">
-        <div className="grid gap-3">
-          {AvailableOAuthProviders.map(provider => (
-            <OAuthLogin key={provider} name={provider} />
-          ))}
-        </div>
-        <div className="relative inset-x-0 my-8 h-[2px] rounded-md bg-[#666] bg-opacity-25 uppercase">
-          <span className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 rounded-md bg-white px-6 py-1 font-bold text-[#666]">
-            Or
-          </span>
-        </div>
+        {AvailableOAuthProviders.length > 0 && (
+          <>
+            <div className="grid gap-3">
+              {AvailableOAuthProviders.map(provider => (
+                <OAuthLogin key={provider} name={provider} />
+              ))}
+            </div>
+            <div className="relative inset-x-0 my-8 h-[2px] rounded-md bg-[#666] bg-opacity-25 uppercase">
+              <span className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 rounded-md bg-white px-6 py-1 font-bold text-[#666]">
+                Or
+              </span>
+            </div>
+          </>
+        )}
         <CredentialsLogin />
       </section>
     </main>
