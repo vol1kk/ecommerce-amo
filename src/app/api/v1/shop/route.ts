8a1cb656c5dc30a4227da07de92a08b05797ce39
Vsevolod <thebrokennworld@gmail.com>
2023-12-21 16:52:49 +0200

    fix: filtering by category works again

--- src/app/api/v1/shop/route.ts
+++ src/app/api/v1/shop/route.ts
@@ -1,11 +1,12 @@
 import prisma from "@/lib/prisma";
+
 export async function GET(request: Request) {
   const url = new URL(request.url);
-  const category = url.searchParams.get("category");
+  const category = url.searchParams.get("category")!;
 
   const res = await prisma.item.findMany({
+    ...(category !== "undefined" && { where: { category: category } }),
     include: { details: { include: { comments: true } } },
-    ...(category && { where: { category } }),
   });
 
   return Response.json(res);
