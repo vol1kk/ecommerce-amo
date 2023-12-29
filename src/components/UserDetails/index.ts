e4a9d9809d8bcea70ca4866d6141c4a925a56052
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-12-29 11:49:53 +0200

    refactor: deleted unused files

--- src/components/UserDetails/index.ts
+++ src/components/UserDetails/index.ts
@@ -2,7 +2,6 @@ export { default as hideDetails } from "@/components/UserDetails/utils/hideDetai
 export { default as getFormDataStr } from "@/components/UserDetails/utils/getFormDataStr";
 
 export { updateUserAction } from "@/components/UserDetails/actions/updateUserAction";
-export { updatePasswordAction } from "@/components/UserDetails/actions/updatePasswordAction";
 
 export { default as Details } from "@/components/UserDetails/components/common/Details";
 export { default as DetailsForm } from "@/components/UserDetails/components/common/DetailsForm";
